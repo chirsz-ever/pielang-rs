f0bd4d96890390b6e13be9a73e655831838c5095
chirsz-ever <chirsz-ever@outlook.com>
2026-07-09 15:27:31 +0800

    更新 LALRPOP 语法, 正确解析 Atom 字面量; 增加测试

--- src/test.rs
+++ src/test.rs
@@ -159,20 +159,44 @@ n
 fn parse_expression() {
     let parser = crate::syntax::ExprParser::new();
     let exprs = [
+        // Nat literals
         "0",
         "1",
         "9876",
+        "01",
+        // FIXME: Pie 拒绝了 -1
+        // "-1",
+
+        // Atom literals
         "'a",
-        "n",
+        "'-a",
+        "'a-",
+        "'atom",
+        "'this-is-a-symbol",
+        "'  btom",
+        "(quote ctom)",
+        "(quote this-is-a-symbol)",
+        "(quote 'a)",
+        // symbols
+        "nil",
+        // S-expressions
+        "(the (List Nat) nil)",
+        "(the(List Nat)nil)",
+        "(cons 2 (same 2))",
         "(lambda (x) x)",
-        "(the (= Nat 0 0) sole)",
-        "(Sigma ((x Nat)(y Nat)) (= Nat x y))",
-        // "'this-is-a-symbol",
+        "(λ (x) (add1 x))",
+        r"(the (Σ ((n Nat))
+         (= Nat n n))
+    (cons 2 (same 2)))",
+        // brackets and braces
+        "[the Nat 1]",
+        "{the Nat 1}",
     ];
     for e in exprs {
-        insta::assert_debug_snapshot!(
-            format!("parse_expression_{}", e),
-            parser.parse(e).expect("parse expression")
-        );
+        insta::with_settings!({
+            description => e,
+        }, {
+            insta::assert_debug_snapshot!(format!("parse_expression_{}", e), parser.parse(e),);
+        });
     }
 }
