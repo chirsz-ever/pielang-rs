52307c351543f8ff8e3e2c5daf8b390059804f88
chirsz-ever <chirsz-ever@outlook.com>
2026-08-17 16:35:50 +0800

    remove unused declarations

--- pielang/src/lib.rs
+++ pielang/src/lib.rs
@@ -3,10 +3,6 @@
 
 use lalrpop_util::lalrpop_mod;
 
-/// Stable-compatible stand-in for the never type `!`.
-/// Used as a phantom type parameter where no meta-info is needed.
-pub type Never = std::convert::Infallible;
-
 lalrpop_mod!(#[allow(clippy::type_complexity)] pub syntax);
 
 pub mod ast;
@@ -14,5 +10,3 @@ pub mod core;
 
 pub mod type_check;
 pub mod utils;
-
-pub use utils::Ref;
